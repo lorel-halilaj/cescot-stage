7fc1543a20a4a8b01fdf0e1cb7303dd3f5a096c0
Lorel <halilaj.lorel@gmail.com>
2026-02-18 11:13:17 +0100

    Add initial Python script for robot interaction

---
+++ python/first-project.py
@@ -0,0 +1,12 @@
+robot_name = "Chappie"
+robot_age = 1
+
+print("Ciao! Il mio nome è " + robot_name + " e ho " + str(robot_age) + " anno.")
+
+usern_name = input("Tu come ti chiami?")
+print("Ciao " + usern_name + "!")
+
+user_age = int(input("Tu quanti hanni hai?"))
+age_difference = user_age - robot_age
+print(str(user_age)+ " anni!? Sono " + str(age_difference) + " anni più di me!")
+print("A presto!")
