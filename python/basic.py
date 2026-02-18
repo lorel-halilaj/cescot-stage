04ffdb1292554f9dad4b27c6c23a7f39e3d6917f
Lorel <halilaj.lorel@gmail.com>
2026-02-18 11:13:08 +0100

    Add basic Python operations and type checking examples

---
+++ python/basic.py
@@ -0,0 +1,32 @@
+# questo è un commento
+print("Hello world!")
+
+# Inizializziamo una variabile
+x = 2
+print(x)
+
+# Operatori numerici
+somma = 8 + 5
+diff = 8 - 5
+molt = 8 * 5
+div = 40 / 8
+floor = 42 / 5
+print(somma)
+print(diff)
+print(molt)
+print(div)
+print(floor)
+
+# Calcolo esponenziale 
+esp = 3 ** 3
+print(esp)
+
+# Divisione intera (floor division)
+floor_division = 30 // 7
+print(floor_division)
+
+# La funzione isinstance() viene utilizzata per verificare se un oggetto è di un certo tipo o se è una sottoclasse di quel tipo
+# isinstance(object, type)
+x = 34
+isinstance(x, int) #output True
+isinstance(x, str) #output False
