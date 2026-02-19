c7c2503a60dff5c9305141398dd5a7810021f9e4
Lorel <halilaj.lorel@gmail.com>
2026-02-19 10:13:09 +0100

    added example for calculating string length

--- python/basic.py
+++ python/basic.py
@@ -37,6 +37,17 @@ arance_per_sacco * 3 #output '131313'
 int(arance_per_sacco) * 3 #output 39
 float(age) #output 26.0
 
+# Calcolare la lunghezza di un dato
+whetting_your_appetite = """Python is an easy to learn, powerful
+programming language. It has efficient high-level data structures and
+a simple but effective approach to object-oriented
+programming. Python’s elegant syntax and dynamic typing, together with
+its interpreted nature, make it an ideal language for scripting and
+rapid application development in many areas on most platforms."""
+
+# Enter your code below:
+print(len(whetting_your_appetite))
+
 # Operatori numerici
 somma = 8 + 5
 diff = 8 - 5
