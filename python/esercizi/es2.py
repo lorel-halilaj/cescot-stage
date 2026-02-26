bb9213d34e0b9c192e5780ca55183840cd7b50cf
Lorel <halilaj.lorel@gmail.com>
2026-02-26 11:42:59 +0100

    fixed es2.py

--- python/esercizi/es2.py
+++ python/esercizi/es2.py
@@ -2,19 +2,10 @@
 Scrivi un programma che chieda tre numeri a, b, c all'utente e mostri il più grande tra loro.
 
 """
-def maggiore(a, b, c):
-    """
-    Questa funzione chiede all'utente tre numeri (a, b e c) e restituisce il maggiore.
 
-    :param a: Primo numero
-    :param b: Secondo numero
-    :param c: Terzo numero
-    
-    """
-    a = input("Inserisci il primo numero: ")
-    b = input("Inserisci il secondo numero: ")
-    c = input("Inserisci il terzo numero: ")
-    maggiore = max(a, b, c)
-    return maggiore
+a = input("Inserisci il primo numero: ")
+b = input("Inserisci il secondo numero: ")
+c = input("Inserisci il terzo numero: ")
+maggiore = max(a, b, c)
 
-print("Il numero maggiore è " + maggiore)
+print("Il numero maggiore è " + str(maggiore))
