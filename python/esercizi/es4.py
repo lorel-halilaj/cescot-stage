e95cdb439d14e4fb7d24ae6a88c879eaf5e89563
Lorel <halilaj.lorel@gmail.com>
2026-02-26 12:13:44 +0100

    added comment to es4 to suggest an alternative way to define vocals

--- python/esercizi/es4.py
+++ python/esercizi/es4.py
@@ -4,6 +4,7 @@ Scrivi un programma che chieda all'utente una stringa composta da un solo caratt
 """
 
 vocals = ["a", "e", "i", "o", "u"]
+# oppure vocals = "aeiou"
 user_letter = input("Inserisci una lettera: ").lower()
 if len(user_letter) > 1:
     print("Hai inserito più di una lettera.")
