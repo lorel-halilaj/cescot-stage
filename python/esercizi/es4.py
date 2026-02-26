fc977bfc62a217c0a14b3864f9cdfbf6054464ae
Lorel <halilaj.lorel@gmail.com>
2026-02-26 11:57:09 +0100

    added es4.py that detects if the letter given by the user is a vocal

---
+++ python/esercizi/es4.py
@@ -0,0 +1,14 @@
+"""
+Scrivi un programma che chieda all'utente una stringa composta da un solo carattere e dica se si tratta di una vocale oppure no.
+
+"""
+
+vocals = ["a", "e", "i", "o", "u"]
+user_letter = input("Inserisci una lettera: ").lower()
+if len(user_letter) > 1:
+    print("Hai inserito più di una lettera.")
+else:
+    if user_letter in vocals:
+        print("La lettera " + user_letter.upper() + " è una vocale.")
+    else:
+        print("La lettera " + user_letter.upper() + " non è una vocale.")
